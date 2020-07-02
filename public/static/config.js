b652aa758e86cd345a933d7f4472e8e31a8f4888
dream1009 <1464619327@qq.com>
2020-07-02 17:50:01 +0800

    修改

--- public/static/config.js
+++ public/static/config.js
@@ -1,7 +1,6 @@
 window.config = {
   TIME_OUT: 60000,
-  BASE_URL: "http://2140u809i6.imwork.net:57223/ibpsApi/api",
-  // BASE_URL: "http://svr07.sz-hkcw.com:8083/ibpsApi/api",
+  BASE_URL: "",
   THEME_COLOR: "#288ef0",
   TEXT_COLOR: "white",
   ICON_COLOR: "white",
